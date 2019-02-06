4fa569c1a8b367b0bb07b7fa1b1bfef3c01ca880
kristinn <kristinn@landsbokasafn.is>
2019-02-06 13:37:52 +0000

    Add required record seperators. Track bytes written.

--- src/org/netpreserve/jwarc/WarcWriter.java
+++ src/org/netpreserve/jwarc/WarcWriter.java
@@ -8,25 +8,41 @@ package org.netpreserve.jwarc;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class WarcWriter {
     private final WritableByteChannel channel;
     private final WarcCompression compression;
     private final ByteBuffer buffer = ByteBuffer.allocate(8192);
+    ByteBuffer recordSeperator = ByteBuffer.allocate(2);
+    
+    private AtomicLong bytesWritten = new AtomicLong(0);
 
     public WarcWriter(WritableByteChannel channel, WarcCompression compression) {
+        if (compression == WarcCompression.GZIP) {
+            throw new UnsupportedOperationException("Writing of GZIP WARC files is not currently supported");
+        }
         this.channel = channel;
         this.compression = compression;
+        
+        recordSeperator.put(new byte[] { '\n','\n' });
     }
 
     public synchronized void write(WarcRecord record) throws IOException {
         // TODO: buffer headers, compression
-        channel.write(ByteBuffer.wrap(record.serializeHeader()));
+        bytesWritten.addAndGet(channel.write(ByteBuffer.wrap(record.serializeHeader())));
         MessageBody body = record.body();
         while (body.read(buffer) >= 0) {
             buffer.flip();
-            channel.write(buffer);
+            bytesWritten.addAndGet(channel.write(buffer));
             buffer.compact();
         }
+        recordSeperator.rewind();
+        bytesWritten.addAndGet(channel.write(recordSeperator));
+                
+    }
+    
+    public long getCurrentWritePosition() {
+        return bytesWritten.get();
     }
 }
