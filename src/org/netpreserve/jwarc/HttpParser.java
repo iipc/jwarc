iipc/jwarc

commit ce336d16406853108745059bdd33ba8dd2b213af

LenientHttpParser: Accept headers missing terminating linefeeds
We've seen in the wild some Heritrix WARC files where a redirect HTTP
response is missing the terminating CRLF at the end of the headers.
Presumably some servers send it this way when there's no message body.

To support this the parser now needs to know when we hit the end of
input. This is indicated by calling parse(null).

src/org/netpreserve/jwarc/HttpParser.rl
@@ -15,7 +15,7 @@ action end_of_text { endOfText = bufPos; }
 action handle_method  { method = new String(buf, 0, bufPos, US_ASCII); bufPos = 0; }
 action handle_reason  { reason = new String(buf, 0, bufPos, ISO_8859_1); bufPos = 0; }
 action handle_target  { target = new String(buf, 0, bufPos, ISO_8859_1); bufPos = 0; }
-action finish { finished = true; }
+action finish { finished = true; fbreak; }
 
 action fold {
     if (bufPos > 0) {
@@ -94,11 +94,17 @@ field_name_lenient = ((any - '\r' - '\n' - ' ' - '\t' - ':') (any - '\r' - '\n' 
 field_value_first_lenient = OWS (TEXT_lenient OWS)? $push;
 field_value_folded_lenient = LWS (TEXT_lenient OWS)? >fold $push;
 field_value_lenient = field_value_first_lenient (field_value_folded_lenient)*;
-named_field_lenient = (field_name_lenient OWS)? ":" >handle_name field_value_lenient CRLF_lenient %handle_value;
-named_fields_lenient = named_field_lenient* CRLF_lenient;
 
-http_request_lenient := request_line_lenient named_fields_lenient @finish;
-http_response_lenient := status_line_lenient named_fields_lenient @finish;
+named_field_lenient = (field_name_lenient OWS)? ":" >handle_name field_value_lenient CRLF_lenient %handle_value;
+named_fields_normal = named_field_lenient* CRLF_lenient @finish;
+
+named_field_unterminated = (field_name_lenient OWS)? ":" >handle_name field_value_lenient %handle_value;
+named_fields_unterminated = named_field_lenient* named_field_unterminated? %finish;
+
+named_fields_lenient = named_fields_normal | named_fields_unterminated;
+
+http_request_lenient := request_line_lenient named_fields_lenient;
+http_response_lenient := status_line_lenient named_fields_lenient;
 
 }%%
 
@@ -232,15 +238,31 @@ public class HttpParser extends MessageParser {
         initialState = cs;
     }
 
+    /**
+     * Runs the parser on a buffer of data. Passing null as the buffer indicates the end of input.
+     */
     @SuppressWarnings({"UnusedAssignment", "ConstantConditions", "ConditionalBreakInInfiniteLoop"})
     public void parse(ByteBuffer data) {
-        int p = data.position();
-        int pe = data.limit();
+        int p;
+        int pe;
+        int eof;
+
+        if (data == null) {
+            p = 0;
+            pe = 0;
+            eof = 0;
+        } else {
+            p = data.position();
+            pe = data.limit();
+            eof = -1;
+        }
 
         %% write exec;
 
-        position += p - data.position();
-        data.position(p);
+        if (data != null) {
+            position += p - data.position();
+            data.position(p);
+        }
     }
 
     public void parse(ReadableByteChannel channel, ByteBuffer buffer) throws IOException {
@@ -265,7 +287,10 @@ public class HttpParser extends MessageParser {
             }
             buffer.compact();
             int n = channel.read(buffer);
-            if (n < 0) throw new EOFException("state=" + cs);
+            if (n < 0) {
+                parse(null);
+                break;
+            }
             buffer.flip();
         }
     }

test/org/netpreserve/jwarc/HttpParserTest.java
@@ -8,7 +8,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.util.Optional;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
 import static org.junit.Assert.*;
 
 public class HttpParserTest {
@@ -92,13 +92,33 @@ public class HttpParserTest {
         assertEquals(Optional.of("yes"), httpParser.headers().sole("<span class=\"nonsense\">nonsense</span>"));
         parse("HTTP/1.1 200 OK\n: yes  \n\n");
         assertEquals(Optional.of("yes"), httpParser.headers().sole(""));
+        parse("HTTP/1.1 200\r\nKey: value");
+        assertEquals(Optional.of("value"), httpParser.headers().sole("Key"));
+        parse("HTTP/1.1 200\r\nKey: value\r\nkey2: value2\r\n");
+        assertEquals(Optional.of("value2"), httpParser.headers().sole("key2"));
     }
 
-    private void parse(String message) throws IOException {
+    @Test
+    public void shouldStopBeforeBody() throws IOException {
+        httpParser.lenientResponse();
+        assertEquals(6, parse("HTTP/1.1 200\r\nContent-Length: 0\r\n\r\n[body]"));
+        assertEquals(6, parse("HTTP/1.1 200\r\nContent-Length: 0\n\n[body]"));
+        httpParser.strictResponse();
+        assertEquals(6, parse("HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n[body]"));
+        httpParser.strictRequest();
+        assertEquals(6, parse("GET / HTTP/1.1\r\n\r\n[body]"));
+        httpParser.lenientRequest();
+        assertEquals(6, parse("GET / HTTP/1.1\r\n\r\n[body]"));
+        assertEquals(6, parse("GET /\n\n[body]"));
+    }
+
+    private int parse(String message) throws IOException {
         httpParser.reset();
-        httpParser.parse(Channels.newChannel(new ByteArrayInputStream(new byte[0])), wrap(message));
+        ByteBuffer buffer = wrap(message);
+        httpParser.parse(Channels.newChannel(new ByteArrayInputStream(new byte[0])), buffer);
         assertFalse(httpParser.isError());
         assertTrue(httpParser.isFinished());
+        return buffer.remaining();
     }
 
     private void parseShouldFail(String message) throws IOException {
@@ -108,6 +128,6 @@ public class HttpParserTest {
     }
 
     private ByteBuffer wrap(String message) {
-        return ByteBuffer.wrap(message.getBytes(UTF_8));
+        return ByteBuffer.wrap(message.getBytes(ISO_8859_1));
     }
 }

commit d35cfe9816c755983635de4f3a1398a464df2d43

HttpParser: Fix pushback exception when header missing final CRLF
We need to flip the buffer before returning otherwise the buffer is not
in the state the caller expects and we get a 'pushback would result in
negative position' exception. Previously this didn't matter as we were
throwing an EOFException anyway but now EOF is not always an error.

Fixes: ce336d16406853108745059bdd33ba8dd2b213af

src/org/netpreserve/jwarc/HttpParser.java
@@ -142,6 +142,9 @@ public class HttpParser extends MessageParser {
         initialState = cs;
     }
 
+    /**
+     * Runs the parser on a buffer of data. Passing null as the buffer indicates the end of input.
+     */
     @SuppressWarnings({"UnusedAssignment", "ConstantConditions", "ConditionalBreakInInfiniteLoop"})
     public void parse(ByteBuffer data) {
         int p;
@@ -159,7 +162,7 @@ public class HttpParser extends MessageParser {
         }
 
         
-// line 163 "HttpParser.java"
+// line 166 "HttpParser.java"
 	{
 	int _klen;
 	int _trans = 0;
@@ -299,7 +302,7 @@ case 1:
     endOfText = 0;
 }
 	break;
-// line 303 "HttpParser.java"
+// line 306 "HttpParser.java"
 			}
 		}
 	}
@@ -346,7 +349,7 @@ case 4:
     endOfText = 0;
 }
 	break;
-// line 350 "HttpParser.java"
+// line 353 "HttpParser.java"
 		}
 	}
 	}
@@ -356,7 +359,7 @@ case 5:
 	break; }
 	}
 
-// line 258 "HttpParser.rl"
+// line 261 "HttpParser.rl"
 
         if (data != null) {
             position += p - data.position();
@@ -386,11 +389,11 @@ case 5:
             }
             buffer.compact();
             int n = channel.read(buffer);
+            buffer.flip();
             if (n < 0) {
                 parse(null);
                 break;
             }
-            buffer.flip();
         }
     }
 
@@ -402,7 +405,7 @@ case 5:
     }
 
     
-// line 406 "HttpParser.java"
+// line 409 "HttpParser.java"
 private static byte[] init__http_actions_0()
 {
 	return new byte [] {
@@ -654,5 +657,5 @@ static final int http_en_http_request_lenient = 68;
 static final int http_en_http_response_lenient = 1;
 
 
-// line 303 "HttpParser.rl"
+// line 306 "HttpParser.rl"
 }

src/org/netpreserve/jwarc/HttpParser.rl
@@ -287,11 +287,11 @@ public class HttpParser extends MessageParser {
             }
             buffer.compact();
             int n = channel.read(buffer);
+            buffer.flip();
             if (n < 0) {
                 parse(null);
                 break;
             }
-            buffer.flip();
         }
     }
 