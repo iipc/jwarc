440aecedde63b507a6cb4d21f1e9ffdd7fa279ea
Alex Osborne <aosborne@nla.gov.au>
2023-07-10 11:16:10 +0900

    Replace jackson-core with a simple JsonTokenizer class
    This gets us back to being dependency free and adds some memory usage safety limits for good measure.

---
+++ src/org/netpreserve/jwarc/cdx/JsonException.java
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2023 National Library of Australia and the jwarc contributors
+ */
+package org.netpreserve.jwarc.cdx;
+
+class JsonException extends Exception {
+    public JsonException(String message) {
+        super(message);
+    }
+}
