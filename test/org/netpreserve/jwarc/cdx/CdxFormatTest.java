f70a786683d7f3cf2eacc75ea7a0e491a6329398
teg <teg@teg-workstation>
2023-07-12 13:16:42 +0200

    Add support for revisit lines in CDX-indexer
    New option -r or --revisits-included to include revisit for the
    CDX-tool.
    
    The mime-type will be set to 'warc/revisit' to support PyWb.
    (calendar entry as revisit)
    
    The PR closes https://github.com/iipc/jwarc/issues/71

--- test/org/netpreserve/jwarc/cdx/CdxFormatTest.java
+++ test/org/netpreserve/jwarc/cdx/CdxFormatTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.netpreserve.jwarc.HttpResponse;
 import org.netpreserve.jwarc.MediaType;
 import org.netpreserve.jwarc.WarcResponse;
+import org.netpreserve.jwarc.WarcRevisit;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -44,6 +45,26 @@ public class CdxFormatTest {
                 cdxFormat.format(response, "example.warc.gz", 123, 456));
     }
 
-
+    
+    @Test
+    public void testRevisit() throws Exception {
+        CdxFormat cdxFormat = new CdxFormat.Builder()
+                .digestUnchanged() // We want the digest as is.
+                .revisistsIncluded() 
+                .build();
+        String payloadDigest="sha256:b04af472c47a8b1b5059b3404caac0e1bfb5a3c07b329be66f65cfab5ee8d3f3";
+                
+        HttpResponse httpResponse = new HttpResponse.Builder(404, "Not Found")
+                .body(MediaType.HTML, new byte[0])
+                .build();
+        WarcRevisit revisit = new WarcRevisit.Builder("http://example.org/")
+                .date(Instant.parse("2022-03-02T21:44:34Z"))                
+                .body(httpResponse)
+                .addHeader("WARC-Payload-Digest", payloadDigest)
+                .addHeader("WARC-Type", "revisit")
+                .build();
+        assertEquals("org,example)/ 20220302214434 http://example.org/ warc/revisit 404 "+payloadDigest+" - - 456 123 example.warc.gz",                      
+                cdxFormat.format(revisit, "example.warc.gz", 123, 456));
+    }
 
 }
